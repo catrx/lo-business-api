bfe581f463c32d6784517815987059d2cbd6522b
catrix <antonin.catrix@epsi.fr>
2023-06-25 00:35:41 +0200

    FIx: allow cors

--- api/mail.js
+++ api/mail.js
@@ -1,7 +1,24 @@
 const fetch = require("node-fetch");
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
 
-export default async function handler(req, res, next) {
+
+async function handler(req, res, next) {
     const { name, email, sector, message, budget } = req.body;
 
     try {
@@ -48,4 +65,6 @@ export default async function handler(req, res, next) {
         console.error(e)
         res.status(500).send("Error occurred when trying to send email")
     }
-};
+}
+
+module.exports = allowCors(handler)
